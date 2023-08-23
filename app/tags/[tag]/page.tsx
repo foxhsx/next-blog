1b8243829b01ebfa82d943bd1936a06bc002ba8d
foxhsx <932163475@qq.com>
2023-08-23 23:57:50 +0800

    fix: tag 分类错误修复

--- app/tags/[tag]/page.tsx
+++ app/tags/[tag]/page.tsx
@@ -8,7 +8,7 @@ import { genPageMetadata } from 'app/seo'
 import { Metadata } from 'next'
 
 export async function generateMetadata({ params }: { params: { tag: string } }): Promise<Metadata> {
-  const tag = params.tag
+  const tag = decodeURIComponent(params.tag)
   return genPageMetadata({
     title: tag,
     description: `${siteMetadata.title} ${tag} tagged content`,
@@ -36,8 +36,8 @@ export default function TagPage({ params }: { params: { tag: string } }) {
   const title = tag[0].toUpperCase() + tag.split(' ').join('-').slice(1)
   const filteredPosts = allCoreContent(
     allBlogs.filter(
-      (post) => post.draft !== true && post.tags && post.tags.map((t) => slug(t)).includes(tag)
+      (post) => post.draft !== true && post.tags && post.tags.map((t) => slug(t)).includes(decodeURIComponent(tag))
     )
   )
-  return <ListLayout posts={filteredPosts} title={title} />
+  return <ListLayout posts={filteredPosts} title={decodeURIComponent(title)} />
 }
